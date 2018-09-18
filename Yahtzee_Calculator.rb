8d4964a53972dcba8054127abdb9c0ea01b96b23
dylan <dylan.mcclain@gmail.com>
2018-09-18 14:47:11 -0700

    Initialize repository

---
+++ Yahtzee_Calculator.rb
@@ -0,0 +1,222 @@
+=begin
+Yahtzee Calculator
+
+Program by Dylan, June & July 2018
+Current version August 2018
+
+Next Revisions:
+* Use dataset from method parse_dice_and_check to print dice numbers
+    when providing calculations to user (e.g. Your chance of rolling 
+    a straight is x IF YOU KEEP YOUR 2,3,4, and 5)
+* Create a consistent order of outputs that states what you have first,
+    then what your chances of various outcomes are, not intermixed
+* Eliminate the scenario variable and the printing of "scenario we
+    investigated was"
+*Round to nearest whole or half percent
+* Check calculations against another person's yahtzee calcuator
+*BONUS use the point values of outcomes like full house and large straight to
+    make a strategic recommendation to the user.
+*Happy Rolling
+=end
+
+require_relative 'YahtzeeCalculations'
+
+puts %q{Welcome to Yahtzee Calculator v. 2.1
+        Enter your roll by just listing the numbers on the dice.
+        Type 'exit' to exit.
+        Happy rolling!}
+
+class Game
+
+    def initialize
+        @dice = []
+        @roll_count
+        @multiples = {}
+    end
+
+    def start_game
+        puts "\n~~~NEW TURN~~~"
+        puts "What did you roll?"
+        get_dice
+
+        puts "How many rolls do you have left?"
+        get_roll
+
+        organize_dice
+        parse_dice_and_check
+    end
+
+    def get_dice
+        input = gets.chomp
+        if input == 'exit'
+            puts 'bye!'
+            exit
+        end
+
+        @dice = input.to_i.digits.sort
+
+        until @dice.length == 5 && @dice.all? {|x| x.between?(1,6)}
+            puts "Hmmm...that can't be right"
+            puts "What did you really roll?"
+            @dice = gets.to_i.digits.sort
+        end
+    end
+
+    def get_roll
+        input = gets.chomp
+        if input == 'exit'
+            puts 'bye!'
+            exit
+        end
+
+        @roll_count = input.to_i
+
+        until @roll_count.between?(1,2)
+            puts "Well, that's not right.\nIt's either 1 or 2. Which is it?"
+            @roll_count = gets.to_i
+        end
+    end
+    
+    #creates two arrays, one for the numbers you rolled
+    #and one for the amount of times you rolled each number
+    #and then maps those arrays into a hash
+    #then (optional) iterates through the hash to tell player about @multiples
+    def organize_dice
+        unique_numbers = @dice.uniq.each
+        occurrences = []
+        @dice.uniq.each { |n| occurrences << @dice.count(n)}
+        @multiples = unique_numbers.zip(occurrences).to_h
+        #@multiples.each { |k,v| puts "You have #{v} of a kind with #{k}s" }
+    end
+
+    #parses the hash @multiples and then calls the methods that check for
+    #all the possible scenarios
+    def parse_dice_and_check
+        singletons = @multiples.map { |k,v| v==1 ? k : nil }.compact
+        pairs = @multiples.map { |k,v| v==2 ? k : nil }.compact
+        triples = @multiples.map  { |k,v| v==3 ? k : nil }.compact
+        quadruples =  @multiples.map { |k,v| v==4 ? k : nil }.compact
+        yahtzees = @multiples.map { |k,v| v ==5 ? k : nil }.compact
+        check_full_house(singletons, pairs, triples)
+        check_yahtzee(singletons, pairs, triples, quadruples, yahtzees)
+        check_straight
+    end
+    
+    #calculates full house probability within the method
+    #scenarios are: three of a kind or two pair
+    def check_full_house(singletons, pairs, triples)
+        if triples.any? && pairs.empty?
+            scenario = 'full house'
+            chance = (((1.0/6.0) + (5.0/36.0)*(@roll_count-1))*100).truncate(2)
+            puts "If you save your #{singletons.join(' or your ')} your chance of rolling a full house is #{chance}%"
+        elsif pairs.length == 2
+            scenario = 'full house'
+            chance = (((1.0/3.0) + (10.0/36.0)*(@roll_count-1))*100).truncate(2)
+            puts "If you save your two pairs your chance of rolling a full house is #{chance}%"
+        elsif pairs.any? && triples.any?
+            puts "You already have a full house!"
+        end
+    end
+
+    def check_yahtzee(singletons, pairs, triples, quadruples, yahtzees)
+        if pairs.any?
+            pairs.each { |pair| puts "You have a pair of " + pair.to_s + "s" }
+            pairs_check(@roll_count)
+        end
+
+        if triples.any?
+            puts "You already have three of a kind!"
+            triples_check(@roll_count)
+        end
+
+        if quadruples.any?
+            puts "You already have four of a kind!"
+            chance = (((1.0/6.0) + (5.0/36.0)*(@roll_count-1))*100).truncate(2)
+            puts "Your chance of rolling a YAHTZEE of #{quadruples[0]}s is #{chance}%"
+        end
+        
+        if yahtzees.any?
+            sleep(1)
+            puts "wait"
+            sleep(1)
+            puts "wait a sec"
+            sleep(1)
+            puts "OMG YOU HAVE YAHTZEE!\nDID YOU NOT NOTICE?!\nSTOP ROLLING AND COLLECT UR POINTS!"
+            sleep(2)
+        end
+    end
+
+    #check for straight draws, call methods from YahtzeeCalculations.rb
+    def check_straight
+        if five_in_a_row?
+            puts "You have a large straight!"
+        elsif four_in_a_row?
+            puts "You already have a small straight!"
+            if (@multiples.keys & [1,6]).any?
+                scenario = 'four keepers, one edge'
+                straight_E(@roll_count)
+            else
+                scenario = 'four keepers, two edges'
+                straight_F(@roll_count)
+            end
+        elsif three_in_a_row?
+            if four_keepers_one_gap?
+                scenario = 'four keepers, one gap'
+                straight_E(@roll_count)
+            elsif (@multiples.keys & [1,6]).any?
+                scenario = 'three keepers, one edge'
+                straight_A(@roll_count)
+            else
+                scenario = 'three keepers, two edges'
+                straight_D(@roll_count)
+            end
+        elsif three_keepers_one_gap?
+            if (@multiples.keys & [1,6]).any?
+                scenario = 'three keepers, one gap, one edge'
+                straight_A(@roll_count)
+            else
+                scenario = 'three keepers, one gap, two edges'
+                straight_B(@roll_count)
+            end
+        elsif three_keepers_two_gaps?
+            scenario = 'three keepers, two gaps'
+            straight_C(@roll_count)
+        else
+            scenario = 'no straight draw'
+        end
+
+        puts "The program determined we should investigate #{scenario}"
+    end
+
+    #methods used in check_straight
+    def three_in_a_row?
+        @multiples.keys.each_cons(3).any? { |a| a[2] - a[0] == 2 }
+    end
+    
+    def four_in_a_row?
+        @multiples.keys.each_cons(4).any? { |a| a[3] - a[0] == 3 } 
+    end
+    
+    def five_in_a_row?
+        @multiples.keys.each_cons(5).any? { |a| a[4] - a[0] == 4 }
+    end
+    
+    def three_keepers_one_gap?
+        @multiples.keys.each_cons(3).any? { |a| a[2] - a[0] == 3 }
+    end
+    
+    def three_keepers_two_gaps?
+        @multiples.keys.each_cons(3).any? { |a| a[2] - a[0] == 4 }
+    end
+    
+    def four_keepers_one_gap?
+        @multiples.keys.each_cons(4).any? { |a| a[3] - a[0] == 4 }
+    end
+    
+end
+
+new_game = Game.new
+
+loop do
+    new_game.start_game
+end
---
+++ Yahtzee_Probability_Calculations.rb
@@ -0,0 +1,186 @@
+def triples_check(roll_count)
+    #transitional array is chance after one roll of being in state with 3, 4, or 5 dice correct
+    transitional_arr = [(25.0/36.0), (10.0/36.0), (1.0/36.0)]
+    if roll_count == 1
+        quads_chance = (transitional_arr[1]*100).truncate(2)
+        yahtz_chance = (transitional_arr[2]*100).truncate(2)
+        puts "Your chance of rolling four of a kind is #{quads_chance}%"
+        puts "Your chance of rolling YAHTZEE is #{yahtz_chance}%"
+    elsif roll_count == 2
+        #calculate chance of getting quadruples on 2nd roll
+        prob_a = transitional_arr[0]*transitional_arr[1]
+        #add to chance of getting quadruples on first roll
+        quads_chance = ((transitional_arr[1] + prob_a)*100).truncate(2)
+        #for yahtzee, chance of getting nothing on roll one and both dice on roll two
+        prob_one = transitional_arr[0]*transitional_arr[2]
+        #chance of getting one dice on roll one and one on roll two
+        prob_two = transitional_arr[1]*(6.0/36.0)
+        #add these to chance of getting both dice first roll
+        yahtz_chance = ((prob_one + prob_two + transitional_arr[2])*100).truncate(2)
+        puts "Your chance of rolling four of a kind is #{quads_chance}%"
+        puts "Your chance of rolling YAHTZEE is #{yahtz_chance}%"
+    end
+end
+
+def pairs_check(roll_count)
+    transitional_arr = [(108.0/216.0),(92.0/216.0),(15.0/216.0),(1.0/216.0)]
+    if roll_count == 1
+        trips_chance = (transitional_arr[1]*100).truncate(2)
+        quads_chance = (transitional_arr[2]*100).truncate(2)
+        yahtz_chance = (transitional_arr[3]*100).truncate(2)
+        puts "Your chance of rolling three of a kind is #{trips_chance}%"
+        puts "Your chance of rolling four of a kind is #{quads_chance}%"
+        puts "Your chance of rolling YAHTZEE is #{yahtz_chance}%"
+    elsif roll_count == 2
+        #add chance of triples on first roll to chance of nothing on first roll and triples on second roll
+        trips_chance = ((transitional_arr[1] + (transitional_arr[0]*transitional_arr[1]))*100).truncate(2)
+        #for quadruples, chance of getting nothing on first roll and quadruples on second roll
+        prob_a = (transitional_arr[0]*transitional_arr[2])
+        #chance of getting triples on first roll and quadruples on second roll
+        prob_b = (transitional_arr[1]*(10.0/36.0))
+        #add these to chance of getting quadruples on first roll
+        quads_chance = ((transitional_arr[2] + prob_a + prob_b)*100).truncate(2)
+        #for yahtzee, chance of getting nothing on first roll and yahtzee on second roll
+        prob_one = transitional_arr[0]*transitional_arr[3]
+        #chance of getting triples on first roll and yahtzee on second roll
+        prob_two = transitional_arr[1]*(1.0/36.0)
+        #chance of getting quadruples on first roll and yahtzee on second roll
+        prob_three = transitional_arr[2]*(1.0/6.0)
+        #add these to chance of getting yahtzee on first roll
+        yahtz_chance = ((transitional_arr[3] + prob_one + prob_two + prob_three)*100).truncate(2)
+        puts "Your chance of rolling three of a kind is #{trips_chance}%"
+        puts "Your chance of rolling four of a kind is #{quads_chance}%"
+        puts "Your chance of rolling YAHTZEE is #{yahtz_chance}%"
+    end
+end
+
+#STRAIGHT DRAWS
+#Scenarios(2): three unique dice, one gap, one edge // three unique dice, zero gaps, one edge (stacked)
+#Instances(4): (1,2,4) // (1,3,4) // (3,4,6) // (3,5,6) //// (1,2,3) // (4,5,6)
+def straight_A(roll_count)
+    #Transitional Array (chance after one roll of having 3, 4, or 5 dice correct)
+    transitional_arr = [(20.0/36.0), (14.0/36.0), (2.0/36.0)]
+    transitional_arr_gap_dice = [(25.0/36.0), (9.0/36.0)]
+
+    if roll_count == 1
+        #chance with one roll remaining is same as the transitional array
+        #small straight uses separate array because only the gap dice qualifies, not the edge
+        sm_straight = ((transitional_arr_gap_dice[1])*100).truncate(2)
+        lg_straight = ((transitional_arr[2])*100).truncate(2)
+        puts "Your chance of rolling a small straight is #{sm_straight}%"
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif roll_count == 2
+        #calculate chance of small straight by adding prob of 
+        #getting it on the second roll to prob of getting it on first roll
+        prob_a = transitional_arr[0]*transitional_arr_gap_dice[1]
+        sm_straight = ((prob_a + transitional_arr_gap_dice[1])*100).truncate(2)
+        #for large straight, chance of getting nothing on first roll and both dice on second roll
+        prob_one = transitional_arr[0]*transitional_arr[2]
+        #chance of getting edge dice on first roll and gap on second roll
+        prob_two = (9.0/36.0)*(1.0/6.0)
+        #chance of getting gap dice on first roll and one of two edges on second roll
+        prob_three = transitional_arr_gap_dice[1]*(2.0/6.0)
+        #add these to chance of getting both dice on first roll
+        lg_straight = ((transitional_arr[2] + prob_one + prob_two + prob_three)*100).truncate(2)
+        puts "Your chance of rolling a small straight is #{sm_straight}%"
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    end
+end
+
+#scenario: three unique dice, one gap, two edges
+#Instances(2): (2,3,5) // (2,4,5)
+def straight_B(roll_count)
+    transitional_arr = [(16.0/36.0), (16.0/36.0), (4.0/36.0)]
+    transitional_arr_gap_dice = [(25.0/36.0), (9.0/36.0)]
+
+    if roll_count == 1
+        sm_straight = ((transitional_arr_gap_dice[1])*100).truncate(2)
+        lg_straight = ((transitional_arr[2])*100).truncate(2)
+        puts "Your chance of rolling a small straight is #{sm_straight}%"
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif roll_count == 2
+        prob_a = transitional_arr[0]*transitional_arr_gap_dice[1]
+        sm_straight = ((prob_a + transitional_arr_gap_dice[1])*100).truncate(2)
+        #chance of nothing on first roll and both dice on second roll
+        prob_one = transitional_arr[0]*transitional_arr[2]
+        #chance of gap dice on first roll and one of two edges on second roll
+        prob_two = transitional_arr_gap_dice[1]*(2.0/6.0)
+        #chance of one of two edges on first roll and gap on second roll
+        prob_three = (20.0/36.0)*(1.0/36.0)
+        #add it to chance of both on first roll
+        lg_straight = ((prob_one + prob_two + prob_three + transitional_arr[2])*100).truncate(2)
+    end
+end
+
+#scenario: three unique dice, two gaps, zero edges
+#Instances(6): (1,2,5) // (1,3,5) // (1,4,5) // (2,3,6) // (2,4,6) // (2,5,6)
+def straight_C(roll_count)
+    if roll_count == 1
+        lg_straight = ((2.0/36.0)*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif roll_count == 2
+        lg_straight = (((12.0/36.0)*(1.0/6.0) + (2.0/36.0))*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    end
+end
+
+
+#scenario: three unique dice, zero gaps, two edges
+#Instances(2): (2,3,4) // (3,4,5)
+def straight_D(roll_count)
+    if roll_count == 1
+        sm_straight = ((20.0/36.0)*100).truncate(2)
+        lg_straight = ((2.0/36.0)*100).truncate(2)
+        puts "Your chance of rolling a small straight is #{sm_straight}%"
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif roll_count == 2
+        sm_straight = (((16.0/36.0)*(20.0/36.0) + (20.0/36.0))*100).truncate(2)
+        #chance of rolling short edge on first roll and long edge on second
+        prob_one = (9.0/36.0)*(1.0/6.0)
+        #chance of rolling long edge on first roll and one of two remaining edges on second
+        prob_two = (9.0/36.0)*(2.0/6.0)
+        #chance of rolling nothing on first roll and both on second
+        prob_three = ((16.0/36.0)*(2.0/36.0))
+        #add to chance of rolling both on first roll
+        lg_straight = ((prob_one + prob_two + prob_three + (2.0/36.0))*100).truncate(2)
+        puts "Your chance of rolling a small straight is #{sm_straight}%"
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    end
+end
+
+#scenarios(2): four unique dice, one gap // four unique dice, zero gaps, one edge, (small straight in hand)
+#Instances(8): (1,2,3,5) // (1,2,4,5) // (1,3,4,5) // (2,3,4,6) // (2,3,5,6) // (2,4,5,6) //// (1,2,3,4) // (3,4,5,6)
+def straight_E(roll_count)
+    if roll_count == 1
+        lg_straight = ((1.0/6.0)*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif
+        roll_count == 2
+        lg_straight = ((((5.0/6.0)*(1.0/6.0)) + (1.0/6.0))*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    end
+end
+
+#scenario: four unique dice, zero gaps, two edges, (small straight in hand)
+#Instances(1): (2,3,4,5)
+def straight_F(roll_count)
+    if roll_count == 1
+        lg_straight = ((2.0/6.0)*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    elsif
+        roll_count == 2
+        lg_straight = (((4.0/6.0)*(2.0/6.0) + (2.0/6.0))*100).truncate(2)
+        puts "Your chance of rolling a large straight is #{lg_straight}%"
+    end
+end
+
+#case of: two consecutive numbers, central
+#Instances(1): (3,4)
+
+#case of: two consecutive numbers, open-ended
+#Instances(2): (2,3) // (4,5)
+
+#case of: two consecutive numbers, close-ended
+#Instances(2): (1,2) // (5,6)
+
+#cases of two non-consecutive numbers
